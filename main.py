da86abd49cab1dbc30d0f5630c908eb5dc4bd3f9
huzr <huzhui.x@gmail.com>
2019-11-30 19:57:50 +0800

    完成实验报告

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# import time
 import args
 import func
 
@@ -6,6 +7,10 @@ if __name__ == "__main__":
         args.ord_g), args.g, args.ya, args.ord_g, args.p)
     yb_solver = func.Pohlig_Hellman_algorithm(func.calc_factors(
         args.ord_g), args.g, args.yb, args.ord_g, args.p)
+
+    ya_solver_pollard = func.pollard_algorithm(args.g, args.ya, args.ord_g, args.p)
+    yb_solver_pollard = func.pollard_algorithm(args.g, args.yb, args.ord_g, args.p)
+
 
     xa = ya_solver.solve()
     xb = yb_solver.solve()
@@ -16,3 +21,17 @@ if __name__ == "__main__":
     print("ya^xb = \n", func.modular_exponent(args.ya, xb, args.p))
     print("yb^xa = \n", func.modular_exponent(args.yb, xa, args.p))
     print("两者相等，它们即DH密钥交换协议中的共同内容")
+
+    # start = time.time()
+    # for i in range(20):
+    #     ya_solver.solve()
+    # end = time.time()
+    # print("使用Pohlig Hellman算法的运行时间：（计算十次）")
+    # print(end - start)
+
+    # start = time.time()
+    # for i in range(20):
+    #     ya_solver_pollard.solve()
+    # end = time.time()
+    # print("使用Pollard rho算法的运行时间：（计算十次）")
+    # print(end - start)
